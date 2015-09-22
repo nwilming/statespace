2152342c9fffbe086293c3699bcd5d5d107a0dd5
Niklas Wilming <niklaswilming@fastmail.fm>
2015-09-22 18:12:27 +0200

    Remove ocupy as dependency from statespace - replaced with pandas.
    This change also implies a change in data format, i.e. other files will change soon too.

--- statespace.py
+++ statespace.py
@@ -6,20 +6,53 @@ Implements state space analysis from:
     Context-dependent computation by recurrent dynamics in prefrontal cortex.
     Nature 503, 78–84 (2013).
 
+This implementation relies on a particular representation of your data. It expects
+a pandas DataFrame with a Multi-Index that encodes your conditions and the unit
+number, and that time is encoded in different columns.
+
+For example a data frame like this one should work:
+  >>> data = arange(4*4*6).reshape((16,6)) # 3 factors (+unit encoding), 4 units, 6 time points
+  >>> units = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4]
+  >>> conditions = [[1,2,1,2]*4, [1,1,2,2]*4, [1,1,1,1]*4, units]
+  >>> index = pd.MultiIndex.from_arrays(conditions, names=['C1', 'C2', 'C3', 'unit'])
+  >>> df = pd.DataFrame(data, index=index)
+
+Which looks like this:
+                0   1   2   3   4   5
+C1 C2 C3 unit
+1  1  1  1      0   1   2   3   4   5
+2  1  1  1      6   7   8   9  10  11
+1  2  1  1     12  13  14  15  16  17
+2  2  1  1     18  19  20  21  22  23
+1  1  1  2     24  25  26  27  28  29
+...
+
+You ask why this particular form? It would be best to completely exploit the
+structure of the data, i.e. MEG data is trial x time x sensor (x freq) cube and
+the dimensions of the cube define an implicit index into the data. Yet, such an
+implicit index requires to remember the meaning of each dimension which is a bad
+thing when you go back to the data a few weeks from now. Modern data structures
+like pandas.DataFrame allow efficient data manipulation while explicitly defining
+the semantics of the index (i.e. named indices and data dimensions). The above
+structure is a compromise between exploiting structure (time is represented
+continuosly) and being able to use pandas. Maybe other projects will allow to
+annotate N-dimensional data soon (xray).
+
+ I might switch unit and time soon. Seems to make more sense to have time as an
+ index. 
 '''
-import sys
+
+import sys, logging
 from sklearn import linear_model
 from numpy import *
 from itertools import product, izip
 from sklearn.decomposition import PCA
-import pylab as plt
 from scipy.ndimage import gaussian_filter
 from scipy.stats import nanmean
-import matplotlib
 from ocupy import datamat
 import patsy
 from named_array import AxisArray
-
+import pandas as pd
 
 def zscore(data):
     '''
@@ -30,46 +63,51 @@ def zscore(data):
         sigma = nanstd(data.data[data.unit == n])
         data.data[data.unit == n] = (data.data[data.unit == n] - mu) / sigma
 
+def regression_weights(data, formula):
+    '''
+    Estimate linear weights to predict a unit's activity from predictors.
 
-def patsy_regression_weights(data, formula):
+    data is a pandas.DataFrame that encodes time as colums and conditions and
+    units in it's index (i.e. a multi-index). The index needs to have the levels
+    'trial' and 'unit', the latter encodes the source of a data point ('sensor',
+    'cell' etc.)
+
+    formula is patsy formula description to build a design matrix. The formula
+    language is very similar to R's formula language.
+
+    Output:
+        betas_nt : pandas.DataFrame
+            Encodes for each unit and time point the regression weights for all
+            factors occuring in the formula.
     '''
-    Regress factors onto predicted variable from data.
-    '''
-    ntime = data.data.shape[1]
-    dm = datamat.AccumulatorFactory()
-    for n in unique(data.unit):
-        sys.stdout.flush()
-        unit = data[data.unit == n]
-        datadict = dict((f, unit.field(f)) for f in unit.fieldnames())
-        regs = patsy.dmatrix(formula, data=datadict)
-        move_intercept = False
+    ntime = data.shape[1]
+
+    def get_weights(group):
+        regs = patsy.dmatrix(formula, data=group.reset_index())
         labels = regs.design_info.column_names
-        if labels[0] == 'Intercept':
-            # If an intercept is included it becomes the first term - this is
-            # really counter intuitive for the QR decomposition which is
-            # sensitive to the ordering of variables.
-            labels = labels[1:]+ [labels[0]]
-            move_intercept = True
         coefs = []
-        for t in range(ntime):
-            r_tu = unit.data[:, t]
-            idx = isnan(r_tu)
+        for time in group:
+            data = group[time]
+            idx = isnan(data.values)
+            next_entry = dict((name, nan) for name in labels)
+            next_entry['time'] = time
             if not all(idx):
                 fit = linear_model.LinearRegression(
-                    fit_intercept=False).fit(regs[~idx, :], r_tu[~idx])
-                cs = fit.coef_
-                if move_intercept:
-                    cs[0:-1], cs[-1] = cs[1:], cs[0]
-                cs.dtype = [(name, 'float') for name in labels]
-                coefs.append(cs)
-            else:
-                coefs.append(nan*ones((regs.shape[1],)))
-        dm.update({'unit': n, 'coef': squeeze(array(coefs))})
-    betas_nt = dm.get_dm()
-    return betas_nt, labels
+                    fit_intercept=False).fit(regs[~idx, :], data.values[~idx])
+                for name, value in zip(labels, fit.coef_):
+                    next_entry[name] = value
+            coefs.append(next_entry)
+        df = pd.DataFrame(coefs)
+        return df.set_index(df.time)
 
+    betas_nt = data.groupby(level='unit').apply(get_weights)
+    del betas_nt['time']
+    return betas_nt
 
 def predict_unit(unit, formula, bnt):
+    '''
+    Predict activiy of one unit. TODO: Document this.
+    '''
     trial_dict = dict((f, unit.field(f)) for f in unit.fieldnames())
     dmp = patsy.dmatrix(formula, data=trial_dict)
     pred = nan*ones((len(unit), unit.data.shape[1]))
@@ -79,140 +117,129 @@ def predict_unit(unit, formula, bnt):
     return pred
 
 
-def conmean(dm, **kwargs):
-    '''
-    Computes population average response for condition
-    specified in kwargs.
-    '''
-    for key, value in kwargs.iteritems():
-        dm = dm[dm.field(key) == value]
-    if len(dm) == 0:
-        raise RuntimeError('No data for ' + str(kwargs))
-    return gaussian_filter(nanmean(dm.data, 0), 2)
-
-
-def dict_product(dicts):
-    '''
-    Cartesian products of values in dicts. Returns one dictionary per
-    combination.
-    '''
-    for key, value in dicts.iteritems():
-        try:
-            value[0]
-        except (IndexError, TypeError):
-            dicts[key] = [value]
-    return (dict(izip(dicts, x)) for x in product(*dicts.itervalues()))
-
-
 def condition_matrix(data, conditions):
     '''
-    Computes data matrix X which contains one row per unit and colums
-    contain the concatenated condition averages for this unit.
+    Computes a matrix that contains concatenated time series (across conditions)
+    for each unit. The size of the matrix is Nunit x (Ntime * Nconditions)
 
-    conditions is a list of dictionaries with key:value pairs that determine
-    how to filter the data for each condition.
+    Input:
+        data : pd.DataFrame
+        conditions : list of condition names.
+            The condition names in this list map to *levels* in the dataframe.
+    Output:
+        Matrix : Nunit x (Ntime * Nconditions)
     '''
-    X = []
-    for n in unique(data.unit):
-        uv = []
-        unit = data[data.unit == n]
-        for condition in conditions:
-            p = conmean(unit, unit=[n], **condition)
-            uv.append(p)
-        X.append(concatenate(uv))
-    return array(X)
+    maps = []
+    for _, condition in data.groupby(level=list(['colorc', 'mc', 'trial'])):
+        condition = condition.stack().to_frame()
+        condition.columns = ['data']
+        condition.index.set_names('time', level=-1, inplace=True)
+        m = condition.reset_index().pivot_table(values='data', index='unit', columns='time').values
+        maps.append(m)
+    return hstack(maps)
 
 
-def check_factors(data, factors):
-    for condition in dict_product(factors):
-        x = conmean(data, **condition)
-        if any(isnan(x)):
-            print condition, sum(isnan(x))
-
-
-def pca_cleaning(data, factors, N_components=12):
+def pca_cleaning(X, N_components=12):
     '''
     Performs PCA based data cleaning on the population responses.
 
-    Important: factors is now a list of valid condition dictionaries
-    '''
+    X is a num_units x (time * conditions) matrix that contains in each row
+    the response of a unit. Each response is a unit's time-series in a condition
+    and all conditions relevant to an analysis are concatenated.
 
-    X = condition_matrix(data, factors)
+    The PCA is performed with each column as one observation, the dimensionality
+    of the space is Nunit.
+    '''
     pca = PCA(n_components=min(X.shape[0], N_components))
-    print 'Fitting PCA = %i components' % N_components
-    sys.stdout.flush()
+    logging.info('Fitting PCA = %i components' % N_components)
     pca.fit(X.T)
     ks = []
     for v in pca.components_:
         v = v[:, newaxis]
         ks.append(dot(v, v.T))
     D = array(ks).sum(0)
-    print 'Transforming X'
-    sys.stdout.flush()
+    logging.info('Transforming X')
     Xpca = dot(D, X)
-    return X, Xpca, D, pca.explained_variance_ratio_, pca
+    return Xpca, D, pca.explained_variance_ratio_, pca
 
 
-def regression_embedding(bnt, D):
+def regression_embedding(bnt, D, factors):
     '''
     Find maximum norm regresion coefficients over time, project this into
     PCA cleaned space and orthogonalize regression vectors.
+
+    Returns:
+        Q : AxisArray (array with named colums)
+            Orthogonalized axes in state space.
+        Bmax : AxisArray
+            Non-orthogonalized axes in state space.
+        bmax_list : list
+            List of timepoints where Bmax occurs
+        norms : list
+            Norm of axes vectors, time given by index.
+        maps :  list
+            Coefficients for each unit and time point.
     '''
-    Bmax = []
-    bmax_list = []
-    norms = []
-    maps = []
-    labels = []
-    for v in bnt.coef.dtype.names:
-        b = bnt.coef[v]
-        b = [dot(D, b[:, i]) for i in range(b.shape[1])]
+    Bmax, bmax_list, norms, maps, = [], [], [], []
+    for factor in factors:
+        coefs = bnt[factor].unstack().values
+        b = [dot(D, coefs[:, i]) for i in range(coefs.shape[1])]
         maps.append(array(b))
         norm_b = [linalg.norm(bb) for bb in b]
         bmax = b[argmax(norm_b)]
         Bmax.append(bmax)
         bmax_list.append(where(b == bmax)[0][0])
         norms.append(norm_b)
-        labels.append(v)
-    Bmax = array(Bmax).T
+    Bmax = AxisArray(Bmax, factors).T
     Q, r = linalg.qr(Bmax)
-    Q = AxisArray(Q, labels)
     return Q, Bmax, bmax_list, norms, maps
 
-
-def embedd(data, formula, valid_conditions, N_components=12):
+def embedd(data, formula, conditions=None, N_components=12):
     '''
     Statespace analysis wrapper function. Use this function for your analysis.
 
     Input:
-    data: ocupy.datamat
-    formula: Patsy formula that defines the regression model.
-
+        data: ocupy.datamat
+        formula: Patsy formula that defines the regression model.
     '''
-    print 'Getting regression weights'
-    sys.stdout.flush()
-    bnt, labels = patsy_regression_weights(data, formula)
-    print 'Doing PCA cleaning'
-    sys.stdout.flush()
-    X, Xpca, D, exp_var, pca = pca_cleaning(data, valid_conditions, N_components)
-    print 'Regression embedding'
-    sys.stdout.flush()
-    Q, Bmax, t_bmax, norms, maps = regression_embedding(bnt, D)
-    return Q, Bmax, labels, bnt, D, t_bmax, norms, maps, exp_var, pca
+    logging.info('Getting regression weights')
+    bnt = regression_weights(data, formula)
+
+    if conditions is None:
+        conditions = [f for f in bnt.columns]
+        # Patsy puts the Intercept into the front, which is usually not what is
+        # wanted for the QR decomposition.
+        if conditions[0] == 'Intercept':
+            conditions = conditions[1:] + [conditions[0]]
+        logging.info("Using this condition ordering: " + ' + '.join(conditions))
+
+    logging.info('Building condition matrix')
+    print data.groupby(level=['mc', 'colorc'])
+    X = condition_matrix(data, set(conditions) - set(['Intercept']))
+
+    logging.info('Doing PCA cleaning')
+    Xpca, D, exp_var, pca = pca_cleaning(X, N_components)
+
+    logging.info('Regression embedding')
+    Q, Bmax, t_bmax, norms, maps = regression_embedding(bnt, D, conditions)
+
+    return Q, Bmax, bnt, D, t_bmax, norms, maps, exp_var, pca
 
 
-def valid_conditions(data, factors):
-    conditions = []
-    for c in dict_product(factors):
-        p = conmean(data, **c)
-        if sum(isnan(p)) > 0:
-            continue
-        conditions.append(c)
-    return conditions
-
-
-def get_trajectory(data, conditions, axis1, axis2, time=None):
+def get_trajectory (data, conditions, axis1, axis2, time=None):
     '''
     Project a condition into the state space.
+
+    Input:
+        data : ocupy.datamat
+            A datamat that contains for each (trial x unit) one entry. The datamat needs
+            to contain a field 'data' that encodes time in the 2nd dimension.
+            The field 'units' encodes the unit number. Long story short, it
+            stores the data in long format.
+        conditions : dict
+            This dict determines which conditions are projected into the state
+            space. The dict contains field names of the datamats as keys and
+            admissible values as values.
     '''
     results = {}
     for i, condition in enumerate(conditions):
@@ -224,94 +251,3 @@ def get_trajectory(data, conditions, axis1, axis2, time=None):
             time = arange(len(vax1))
         results[str(condition)] = (vax1, vax2, time)
     return results
-
-
-def plot_population_activity(data, factors, axis1, axis2,
-                             legend=False, epochs=None):
-    import seaborn as sns
-    conditions = list(dict_product(factors))
-    colors = sns.color_palette('muted', len(conditions))
-    symbols = ['-', '--', '-.', ':']
-    leg = []
-    for i, condition in enumerate(conditions):
-        population_activity = condition_matrix(data, [condition])
-        assert population_activity.shape[1] == data.data.shape[1]
-        vax1 = dot(axis1, population_activity)
-        vax2 = dot(axis2, population_activity)
-        if epochs is None:
-            h = plt.plot(vax1, vax2, color=colors[i])[0]
-            plt.plot(vax1[0], vax2[0], 'o', color=colors[i])
-            plt.plot(vax1[-1], vax2[-1], 'D', color=colors[i])
-        else:
-            for j, (low, high) in enumerate(epochs):
-                h = plt.plot(vax1[low:high],
-                             vax2[low:high], ls=symbols[j % len(symbols)],
-                             color=colors[i])[0]
-                plt.plot(vax1[low], vax2[low], 'o', color=colors[i])
-                plt.plot(vax1[high], vax2[high], 'D', color=colors[i])
-
-        leg.append((h, str(condition)))
-    if legend:
-        plt.legend([l[0] for l in leg], [l[1] for l in leg])
-    return [l[0] for l in leg], [l[1] for l in leg]
-
-
-def trellis_plot(data, Q, labels, condition, Bmax=None):
-    import matplotlib
-    gs = matplotlib.gridspec.GridSpec(3, 3)
-    for k in range(1, Q.shape[1]):
-        for j in range(Q.shape[1]-1):
-            plt.subplot(gs[k-1, j])
-            if k <= j:
-                plt.xticks([])
-                plt.yticks([])
-                plt.xlabel('')
-                plt.ylabel('')
-                continue
-            leg = plot_population_activity(
-                data,
-                condition,
-                Q[:, k], Q[:, j])
-
-            plt.xlabel('%s' % labels[k])
-            plt.ylabel('%s' % labels[j])
-            if Bmax is not None:
-                a = dot(Q[:, [k, j]].T, Bmax[:, [k, j]])
-                plt.plot([-a[1, 0], a[1, 0]], [-a[1, 1], a[1, 1]], 'k--')
-                plt.plot([-a[0, 0], a[0, 0]], [-a[0, 1], a[0, 1]], 'k--')
-                plt.axis('equal')
-    plt.subplot(gs[0, 2])
-    plt.legend(leg[0], leg[1])
-    plt.xticks([])
-    plt.yticks([])
-    plt.xlabel('')
-    plt.ylabel('')
-
-
-def dict_filter(dm, condition):
-    idx = ones(dm.field(dm.fieldnames()[0]).shape).astype(bool)
-    for key, val in condition.iteritems():
-        idx = idx & (dm.field(key) == val)
-    return dm[idx]
-
-
-def characterize_population(data, bnt, factors, Q):
-    conditions = list(dict_product(factors))
-    gs = matplotlib.gridspec.GridSpec(
-            len(conditions) + 1, len(unique(data.unit)))
-    for i, cond in enumerate(conditions):
-        d = dict_filter(data, cond)
-        for iu, u in enumerate(d.by_field('unit')):
-            plt.subplot(gs[i, iu])
-            plt.plot(u.data.T.mean(0), 'k', alpha=0.5)
-            plt.ylim([-max(absolute(plt.ylim())), max(absolute(plt.ylim()))])
-            plt.plot(plt.xlim(), [0, 0], 'k--')
-            plt.xticks([])
-            plt.yticks([])
-            if iu == 0:
-                plt.ylabel(str(cond))
-    for iu, unit in enumerate(unique(data.unit)):
-        plt.subplot(gs[len(conditions), iu])
-        bs = [bnt[unit, t] for t in range(bnt.shape[1])]
-        plt.plot(array(bs)[:, (0, 2)])
-        plt.legend(factors.keys())
